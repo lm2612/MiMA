e56ac02e5e11b809ac32864feeca4a7b269dc8a2
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-04-03 19:10:24 +0100

    Coupled code for pytorch (untested compiling).

--- src/atmos_param/cg_drag/pytorch.f90
+++ src/atmos_param/cg_drag/pytorch.f90
@@ -1,4 +1,6 @@
-module pytorch_mod
+module cg_drag_ML_mod
+
+use constants_mod, only:  PI
 
 ! #ML
 ! Imports primitives used to interface with C
@@ -11,20 +13,82 @@ use ftorch
 implicit none
 private
 
-public    cg_drag 
+public    cg_drag_ML_init, cg_drag_ML_end, cg_drag_ML
+
+!--------------------------------------------------------------------
+!   data used in this module
+!
+!--------------------------------------------------------------------
+!   model    ML model type bound to python
+!
+!--------------------------------------------------------------------
+
+type(torch_module) :: model
 
 
-namelist / cg_drag_nml /         &
-                          cg_drag_freq, cg_drag_offset, &
-                          source_level_pressure, damp_level_pressure
+!--------------------------------------------------------------------
+!--------------------------------------------------------------------
 
 contains
 
-subroutine cg_drag(uuu, vvv, psfc, lat, gwfcng_x, gwfcng_y)
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!
+!                      PUBLIC SUBROUTINES
+!
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+!####################################################################
+
+subroutine cg_drag_ML_init(model_path)
 
   !-----------------------------------------------------------------
-  !    cg_drag returns the x and y gravity wave drag forcing terms
-  !    following calculation using an external neural net.
+  !    cg_drag_ML_init is called from cg_drag_init and initialises
+  !    anything required for the ML calculation of cg_drag such as
+  !    an ML model
+  !
+  !-----------------------------------------------------------------
+  
+  !-----------------------------------------------------------------
+  !    intent(in) variables:
+  !
+  !       model_path    full filepath to the model
+  !
+  !-----------------------------------------------------------------
+  character(len=1024), intent(in)        :: model_path
+  
+  !-----------------------------------------------------------------
+  
+  ! Initialise the ML model to be used
+  model = torch_module_load(c_char_model_path//c_null_char)
+
+end subroutine cg_drag_ML_init
+
+
+!####################################################################
+
+subroutine cg_drag_ML_end
+
+  !-----------------------------------------------------------------
+  !    cg_drag_ML_end is called from cg_drag_end and is a destructor
+  !    for anything used in the ML part of calculating cg_drag such
+  !    as an ML model.
+  !
+  !-----------------------------------------------------------------
+  
+  ! destroy the model
+  call torch_module_delete(model)
+
+end subroutine cg_drag_ML_end
+
+
+!####################################################################
+
+subroutine cg_drag_ML(uuu, vvv, psfc, lat, gwfcng_x, gwfcng_y)
+
+  !-----------------------------------------------------------------
+  !    cg_drag_ML returns the x and y gravity wave drag forcing
+  !    terms following calculation using an external neural net.
   !
   !-----------------------------------------------------------------
   
@@ -50,8 +114,6 @@ subroutine cg_drag(uuu, vvv, psfc, lat, gwfcng_x, gwfcng_y)
   real, dimension(:,:),   intent(in)    :: lat, psfc
   
   real, dimension(:,:,:), intent(out)   :: gwfcng_x, gwfcng_y
-
-  character(len=128), intent(in)        :: model_path
   
   !-----------------------------------------------------------------
 
@@ -62,48 +124,89 @@ subroutine cg_drag(uuu, vvv, psfc, lat, gwfcng_x, gwfcng_y)
   !
   !---------------------------------------------------------------------
 
-  real, dimension(:,:), intent(in)    :: uuu_reshaped, vvv_reshaped
-  real, dimension(:),   intent(in)    :: lat_reshaped, psfc_reshaped
+  real, dimension(:,:), allocatable  :: uuu_flattened, vvv_flattened
+  real, dimension(:,:), allocatable  :: uuu_reshaped, vvv_reshaped
+  real, dimension(:), allocatable    :: lat_reshaped, psfc_reshaped
+
+  integer :: imax, jmax, kmax
+  ! real, parameter :: rad2deg = 180.0/(4.0*ATAN(1.0))
+  real, parameter :: rad2deg = 180.0/PI
 
   integer(c_int), parameter :: dims_2D = 2
-  integer(c_int64_t) :: shape_2D(dims_2D) = (/ 10, 10 /)
-  integer(c_int), parameter :: dims_1D = 1
-  integer(c_int64_t) :: shape_1D(dims_1D) = (/ 10 /)
+  integer(c_int64_t) :: shape_2D(dims_2D)
+  integer(c_int), parameter :: dims_1D = 2
+  integer(c_int64_t) :: shape_1D(dims_1D)
+  integer(c_int), parameter :: dims_out = 2
+  integer(c_int64_t) :: shape_out(dims_out)
 
   ! Set up types of input and output data and the interface with C
-  type(torch_module) :: model
   type(torch_tensor) :: uuu_tensor, vvv_tensor, lat_tensor, psfc_tensor,&
                         gwfcng_x_tensor, gwfcng_y_tensor
   
   !----------------------------------------------------------------
 
   ! reshape tensors as required
-  ! wind becomes (128 * Ncol * 40)
+  imax = size(uuu, 1)
+  jmax = size(uuu, 2)
+  kmax = size(uuu, 3)
 
-  u_flat = magic_flattening_code(uuu)
-  uuu_reshaped = transpose(u_flat)
+  ! Note that the '1D' tensor has 2 dimensions, one of which is size 1
+  shape_2D = (/ imax*jmax, kmax /)
+  shape_1D = (/ imax*jmax, 1 /)
+
+  ! flatten data (nlat, nlon, n) --> (nlat*nlon, n)
+  allocate( uuu_flattened(imax*jmax, kmax) )
+  allocate( vvv_flattened(imax*jmax, kmax) )
+  allocate( uuu_reshaped(kmax, imax*jmax) )
+  allocate( vvv_reshaped(kmax, imax*jmax) )
+  allocate( lat_reshaped(imax*jmax) )
+  allocate( psfc_reshaped(imax*jmax) )
+
+  do j=1,jmax
+      uuu_flattened((j-1)*imax+1:j*imax,:) = uuu(:,j,:)
+      vvv_flattened((j-1)*imax+1:j*imax,:) = vvv(:,j,:)
+      lat_reshaped((j-1)*imax+1:j*imax) = lat(:,j)*rad2deg
+      psfc_reshaped((j-1)*imax+1:j*imax) = psfc(:,j)/100
+  end do
+
+  uuu_reshaped = TRANSPOSE(uuu_flattened)
+  vvv_reshaped = TRANSPOSE(vvv_flattened)
 
   ! Create input/output tensors from the above arrays
-  uuu_tensor = torch_tensor_from_blob(c_loc(uuu_reshaped), dims_2D, shape_2D, torch_kFloat32, torch_kCPU)
-  vvv_tensor = torch_tensor_from_blob(c_loc(vvv_reshaped), dims_2D, shape_2D, torch_kFloat32, torch_kCPU)
+  uuu_tensor = torch_tensor_from_blob(c_loc(uuu_reshaped), dims_2D, shape_2D, torch_kFloat64, torch_kCPU)
+  vvv_tensor = torch_tensor_from_blob(c_loc(vvv_reshaped), dims_2D, shape_2D, torch_kFloat64, torch_kCPU)
+  lat_tensor = torch_tensor_from_blob(c_loc(lat_reshaped), dims_1D, shape_1D, torch_kFloat64, torch_kCPU)
+  psfc_tensor = torch_tensor_from_blob(c_loc(psfc_reshaped), dims_1D, shape_1D, torch_kFloat64, torch_kCPU)
 
-  out_tensor = torch_tensor_from_blob(c_loc(out_data), out_dims, out_shape, torch_kFloat32, torch_kCPU)
+  gwfcng_x_tensor = torch_tensor_from_blob(c_loc(out_data), out_dims, out_shape, torch_kFloat64, torch_kCPU)
+  gwfcng_y_tensor = torch_tensor_from_blob(c_loc(out_data), out_dims, out_shape, torch_kFloat64, torch_kCPU)
 
   ! Load model and Infer
-  model = torch_module_load(c_char_model_path//c_null_char)
-  call torch_module_forward(model, uuu_tensor, gwfcng_x_tensor)
-  call torch_module_forward(model, vvv_tensor, gwfcng_y_tensor)
+  call torch_module_forward(model, uuu_tensor, lat_tensor, psfc_tensor, gwfcng_x_tensor)
+  call torch_module_forward(model, vvv_tensor, lat_tensor, psfc_tensor, gwfcng_y_tensor)
 
 
   ! Convert back into fortran types, reshape, and assign to gwfcng
 
 
   ! Cleanup
-  call torch_module_delete(model)
-  call torch_tensor_delete(in_tensor)
-  call torch_tensor_delete(out_tensor)
-  ! deallocate(in_data)
-  ! deallocate(out_data)
+  call torch_tensor_delete(uuu_tensor)
+  call torch_tensor_delete(vvv_tensor)
+  call torch_tensor_delete(lat_tensor)
+  call torch_tensor_delete(psfc_tensor)
+  call torch_tensor_delete(gwfcng_x_tensor)
+  call torch_tensor_delete(gwfcng_y_tensor)
+  deallocate( uuu_flattened )
+  deallocate( vvv_flattened )
+  deallocate( uuu_reshaped )
+  deallocate( vvv_reshaped )
+  deallocate( lat_reshaped )
+  deallocate( psfc_reshaped )
 
 
-end subroutine cg_drag
+end subroutine cg_drag_ML
+
+
+!####################################################################
+
+end module pytorch_mod
--- src/atmos_param/physics_driver/physics_driver.f90
+++ src/atmos_param/physics_driver/physics_driver.f90
@@ -122,7 +122,7 @@ use radiative_gases_mod,     only: radiative_gases_init,   &
 
 use damping_driver_mod,      only: damping_driver,      &
                                    damping_driver_init, &
-                                   damping_driver_end
+                                   damping_driver_end, &
 
 use grey_radiation_mod, only: grey_radiation_init, grey_radiation, grey_radiation_end
 
@@ -1303,11 +1303,11 @@ real,  dimension(:,:,:), intent(out)  ,optional :: diffm, difft
       z_pbl(:,:) = pbltop(is:ie,js:je) 
       if(do_damping) then
         call mpp_clock_begin ( damping_clock )
-        call damping_driver (is, js, lat, Time_next, dt,           &
+        call damping_driver (is, ie, js, je, lat, Time_next, dt,           &
                              p_full, p_half, z_full, z_half,          &
                              um, vm, tm, qm, rm(:,:,:,1:ntp), &
                              udt, vdt, tdt, qdt, rdt,&
-                             z_pbl , mask=mask, kbot=kbot)
+                             z_pbl, mask=mask, kbot=kbot)
        call mpp_clock_end ( damping_clock )
      endif
 
