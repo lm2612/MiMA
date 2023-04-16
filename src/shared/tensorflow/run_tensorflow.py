9b67fc1a38a7e4a5ce122c652a6b8eba8a63d8ed
Simon Clifford <sjc306@cam.ac.uk>
2023-04-16 12:56:19 +0100

    Working forpy + Tensorflow

---
+++ src/shared/tensorflow/run_tensorflow.py
@@ -0,0 +1,68 @@
+"""
+Contains all python commands MiMA will use.
+
+This module will use the TensorFlow model.
+
+"""
+import tensorflow as tf
+
+
+# Initialize everything
+def initialize(*args, **kwargs):
+    """
+    Initialize a WaveNet model and load weights.
+
+    Parameters
+    __________
+    ignores all parameters
+
+    """
+
+    model = tf.keras.models.load_model('/home/sjc306/hpc-work/Datawave/MiMA/src/shared/tensorflow/saved_model')
+
+    return model
+
+
+# Compute drag
+def compute_reshape_drag(*args):
+    """
+    Compute the drag from inputs using a neural net.
+
+    Takes in input arguments passed from MiMA and outputs drag in shape desired by MiMA.
+    Reshaping is performed in the calling Fortran.
+
+    Parameters
+    __________
+    model :
+        WaveNet model ready to be deployed.
+    wind :
+        U or V (128, num_col, 40)
+    lat :
+        latitudes (num_col)
+    p_surf :
+        surface pressure (128, num_col)
+    Y_out :
+        output prellocated in MiMA (128, num_col, 40)
+    num_col :
+        # of latitudes on this proc
+
+    Returns
+    -------
+    Y_out :
+        Results to be returned to MiMA
+    """
+    model, wind, lat, p_surf, Y_out, num_col = args
+
+    # Make tensors
+    # TF will make Tensors itself?
+    #wind_T = tensor(wind)
+    #lat_T = tensor(lat)
+    #pressure_T = tensor(p_surf)
+
+    # Apply model.
+    temp = model.predict([wind, lat, p_surf])
+
+    # Place in output array for MiMA.
+    Y_out[:, :] = temp
+
+    return Y_out
